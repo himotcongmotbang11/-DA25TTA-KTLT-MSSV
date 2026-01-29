88bf236c23f6f46ebb8c03430d89fd7ffbe45af8
himotcongmotbang11 <trongconga7@gmail.com>
2026-01-29 07:43:43 +0700

    Add files via upload

---
+++ nguyenhuynhnhuy110125283.cpp
@@ -0,0 +1,24 @@
+# include<stdio.h>
+ int main ()
+ {
+ 	int n, m;
+ 	int tong=0;
+ 	int hieu=0;
+ 	int tich=1;
+ 	float thuong=1;
+ 	
+ 	
+ 	printf ("nhap mot so nguyen duong n = ");
+ 	scanf("%d",&n);
+ 	printf ("nhap mot so nguyen duong n = ");
+ 	scanf("%d",&m);
+	tong= m+n;
+	hieu= n-m;
+	tich= n*m;
+	thuong=n/m;
+ 	printf ("\ntong hai so nguyen duong n = %d",tong);
+ 	printf ("\nhieu hai so nguyen duong n = %d",hieu);
+ 	printf ("\ntich hai so nguyen duong n =%d ",tich);
+ 	printf ("\nthuong so nguyen duong n =%.2f ",thuong);
+ 	
+ }
